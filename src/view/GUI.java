e4fe7a56ed3d3a8f5277c33e8d2eb79c82435a36
Christian <chrisabc65@gmail.com>
2023-06-12 09:53:14 +0200

    Add: Moduswechsel Change: einzelne Codeoptimierungen

--- src/view/GUI.java
+++ src/view/GUI.java
@@ -5,29 +5,10 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import java.awt.GridLayout;
 import javax.swing.JButton;
 import javax.swing.JMenuBar;
-import javax.swing.JMenu;
 import javax.swing.JLabel;
-import java.awt.BorderLayout;
 import java.awt.Font;
-import javax.swing.SwingConstants;
-import javax.swing.JLayeredPane;
-import java.awt.GridBagLayout;
-import javax.swing.JToolBar;
-import javax.swing.JSplitPane;
-import javax.swing.JTabbedPane;
-import javax.swing.JInternalFrame;
-import com.jgoodies.forms.layout.FormLayout;
-import com.jgoodies.forms.layout.ColumnSpec;
-import com.jgoodies.forms.layout.FormSpecs;
-import com.jgoodies.forms.layout.RowSpec;
-import java.awt.FlowLayout;
-import java.awt.GridBagConstraints;
-import net.miginfocom.swing.MigLayout;
-import javax.swing.JList;
-import javax.swing.JSpinner;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
 
@@ -57,52 +38,67 @@ public class GUI extends JFrame {
 	public GUI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 200);
-		
-		JMenuBar menuBar = new JMenuBar();
-		setJMenuBar(menuBar);
-		
-		JButton btnSudoku = new JButton("Sudoku");
-		menuBar.add(btnSudoku);
-		
-		JButton btnStr8 = new JButton("Str8");
-		menuBar.add(btnStr8);
-		
-		JButton btnKiller = new JButton("Killer");
-		menuBar.add(btnKiller);
+
+
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
-		JLabel lblNewLabel = new JLabel("Sudoku Rechner");
-		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 16));
-		lblNewLabel.setBounds(124, 11, 180, 40);
-		contentPane.add(lblNewLabel);
-		
-		JLabel lblNewLabel_1 = new JLabel("Sudoku lösen lassen");
-		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		lblNewLabel_1.setBounds(63, 62, 127, 14);
-		contentPane.add(lblNewLabel_1);
-		
-		JLabel lblNewLabel_2 = new JLabel("Sudoku erstellen");
-		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblNewLabel_2.setBounds(63, 92, 127, 14);
-		contentPane.add(lblNewLabel_2);
-		
+
+		JLabel lblTitle = new JLabel("Sudoku Rechner");
+		lblTitle.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblTitle.setBounds(124, 11, 180, 40);
+		contentPane.add(lblTitle);
+
+		JLabel lblSolveType = new JLabel("Sudoku lösen lassen");
+		lblSolveType.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblSolveType.setBounds(63, 62, 127, 14);
+		contentPane.add(lblSolveType);
+
+		JLabel lblCreateType = new JLabel("Sudoku erstellen");
+		lblCreateType.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblCreateType.setBounds(63, 92, 127, 14);
+		contentPane.add(lblCreateType);
+
 		JButton btnLeeresOeffnen = new JButton("Feld öffnen");
 		btnLeeresOeffnen.setBounds(315, 59, 102, 23);
 		contentPane.add(btnLeeresOeffnen);
-		
+
 		JComboBox comboBox = new JComboBox();
 		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Leicht", "Mittel", "Schwer"}));
 		comboBox.setBounds(200, 89, 89, 22);
 		contentPane.add(comboBox);
-		
+
 		JButton btnErstellen = new JButton("Erstellen");
 		btnErstellen.setBounds(315, 89, 102, 23);
 		contentPane.add(btnErstellen);
-		
-		
+
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+
+		JButton btnModeSudoku = new JButton("Sudoku");
+		btnModeSudoku.addActionListener(event -> {
+			lblTitle.setText("Sudoku Rechner");
+			lblCreateType.setText("Sudoku erstellen");
+			lblSolveType.setText("Sudoku lösen lassen");
+		});
+		menuBar.add(btnModeSudoku);
+
+		JButton btnModeStr8 = new JButton("Str8");
+		btnModeStr8.addActionListener(event -> {
+			lblTitle.setText("Str8 Rechner");
+			lblCreateType.setText("Str8 erstellen");
+			lblSolveType.setText("Str8 lösen lassen");
+		});
+		menuBar.add(btnModeStr8);
+
+		JButton btnModeKiller = new JButton("Killer");
+		btnModeKiller.addActionListener(event -> {
+			lblTitle.setText("Killer Rechner");
+			lblCreateType.setText("Killer erstellen");
+			lblSolveType.setText("Killer lösen lassen");
+		});
+		menuBar.add(btnModeKiller);
 	}
 }
