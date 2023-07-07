321d950d1589bd38579bd639a809e6bb306fa713
MaarxS <marvins9204@gmail.com>
2023-07-07 14:53:46 +0200

    add: group selection in gui

--- src/controller/KillerController.java
+++ src/controller/KillerController.java
@@ -2,29 +2,58 @@ package controller;
 
 import java.awt.event.ActionEvent;
 
+import javax.swing.JOptionPane;
+
 import model.KillerField;
 import model.Position;
+import view.KillerFieldGUI;
 import view.SudokuFieldGUI;
 
 public class KillerController extends SudokuController<KillerField>{
 
+	private KillerFieldGUI gui;
+	
 	public KillerController(KillerField killer, KillerField solvedKiller) {
 		super(killer, solvedKiller);
 	}
 	@Override
 	public void setGUI(SudokuFieldGUI killerFieldGUI) {
 		super.setGUI(killerFieldGUI);
-
+		gui = (KillerFieldGUI) killerFieldGUI;
 	}
+	
+	public void onAddGroup(ActionEvent event) {
+		String input = gui.showNumberDialog();
+		if (input == null) {
+			return;
+		}
+		try {
+			int sum = Integer.parseInt(input);
+			if (sum <= 0) {
+				JOptionPane.showMessageDialog(null, "Die Summe muss mindestens 1 sein");
+				return;
+			}
+			if (sum >= 100) {
+				JOptionPane.showMessageDialog(null, "Die Summe muss kleiner als 100 sein.");
+				return;
+			}
+			// TODO get group count
+			playerField.setSum(1, sum);
+			for (Position pos : Position.iterateAll()) {
+				if (gui.getColor(pos) == KillerFieldGUI.COLOR_SELECTED) {
+					playerField.setGroup(pos, 1);
+					gui.setColor(pos, KillerFieldGUI.COLOR_BACKGROUND);
+				}
+			}
+		} catch (NumberFormatException e) {
+			JOptionPane.showMessageDialog(null, "Geben Sie eine Zahl ein.");
+		}
+	}
+	
 	@Override
 	public void clearFieldOnClick(ActionEvent e) {
-		for (Position pos : Position.iterateAll()) {
-			if (gui.isEditable(pos)) {
-				gui.setTextfield(pos, "");
-				gui.setColor(pos, SudokuFieldGUI.COLOR_BACKGROUND);
-				playerField.set(pos, 0);
-			}
-		}
+		super.clearFieldOnClick(e);
+		// TODO entferne Ränder
 	}
 
 }
--- src/view/KillerFieldGUI.java
+++ src/view/KillerFieldGUI.java
@@ -1,20 +1,19 @@
 package view;
 
-import java.awt.EventQueue;
+import java.awt.Color;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
+import javax.swing.JButton;
 import javax.swing.JCheckBox;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
+import javax.swing.JOptionPane;
 
 import controller.KillerController;
-import controller.Str8tsController;
 import model.Position;
 
 public class KillerFieldGUI extends SudokuFieldGUI {
 
+	public static final Color COLOR_SELECTED = Color.BLUE;
 	
 	public KillerFieldGUI(KillerController controller, boolean addOn) {
 		super(controller, addOn);
@@ -22,8 +21,28 @@ public class KillerFieldGUI extends SudokuFieldGUI {
 		lblTitleField.setText("Killer");
 		if(addOn) {
 			btnSolve.setText("Killer lösen");
-	
+			JButton btnAddGroup = new JButton("Gruppe hinzufügen");
+			btnAddGroup.addActionListener(controller::onAddGroup);
+			controlPanel.add(btnAddGroup);
+			JCheckBox cbSelectGroup = new JCheckBox("Gruppe markiern");
+			controlPanel.add(cbSelectGroup);
+			for(Position pos : Position.iterateAll()) {
+				fieldPanel.addTextFieldMouseListener(pos, new MouseAdapter() {
+					public void mousePressed(MouseEvent e){
+						if(cbSelectGroup.isSelected()) {
+							if (fieldPanel.getColor(pos).equals(COLOR_BACKGROUND)) {
+								fieldPanel.setColor(pos, COLOR_SELECTED);
+							} else {
+								fieldPanel.setColor(pos, COLOR_BACKGROUND);
+							}
+						}
+					}
+				});
+			}
 		}
 	}
 
+	public String showNumberDialog() {
+		return JOptionPane.showInputDialog("Geben Sie die Summe der Gruppe ein:");
+	}
 }
